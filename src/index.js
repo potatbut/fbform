166e6f942c928c7271d2e51d2fe7eb0817f75a0c
potatbut <darthyoon@gmail.com>
2022-03-01 18:29:45 +0300

    Firebase settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  child,
+  update
+} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBz1aRdT0b7URqp9YNvl482Ou-ng8w-A8w",
@@ -13,9 +21,14 @@ const firebaseConfig = {
   measurementId: "G-9R0YQQM2N5"
 };
 
+const nowdate = new Date().toLocaleTimeString();
+
 const app = initializeApp(firebaseConfig)
 const data = getDatabase(app)
-const list = ref(data, 'users')
+
+
+
+
 
 const form = document.getElementById('form')
 
@@ -25,6 +38,9 @@ form.addEventListener('submit', submit)
 function submit(e) {
   e.preventDefault()
 
+
+  const smth = 'smth'
+
   const firstName = getValue('firstName')
   const lastName = getValue('lastName')
   const email = getValue('email')
@@ -32,23 +48,88 @@ function submit(e) {
   const cb1 = getValue('cb1')
   const cb2 = getValue('cb2')
   const select = getValue('select')
-  saveData(firstName, lastName, email, phone, cb1, cb2, select) 
+  saveData(firstName, lastName, email, phone, cb1, cb2, select, smth)
 }
 
 function getValue(id) {
   return document.getElementById(id).value
 }
 
-function saveData(firstName, lastName, email, phone, cb1, cb2, select) {
-  
-  const newList = push(list)
-  set(newList, {
+
+function saveData(firstName, lastName, email, phone, cb1, cb2, select, smth) {
+
+
+const userid = testWord(firstName)
+
+ const userInfo = {
     firstName: firstName,
     lastName: lastName,
     email: email,
-    phone: phone, 
-    cb1: cb1, 
+    phone: phone,
+    cb1: cb1,
     cb2: cb2,
-    select: select
-  })
+    select: select,
+    smth: smth
+  }
+
+  // const newUsrKey = push(child(ref(data), 'users')).key
+
+  const updates = {}
+  // updates['/smth/' + newUsrKey] = userInfo
+  updates['/users/' + `${userid}` ] = userInfo
+
+  return update(ref(data), updates)
+
+  // const newList = push(list)
+  /* set(list, {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phone: phone,
+    cb1: cb1,
+    cb2: cb2,
+    select: select,
+    smth: smth
+  }) */
 }
+
+
+/* Cyrillic check */
+function translit(word){
+	var answer = '';
+	var converter = {
+		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
+		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
+		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
+		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
+		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
+		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
+		'э': 'e',    'ю': 'yu',   'я': 'ya',
+ 
+		'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
+		'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
+		'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
+		'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
+		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
+		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
+		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
+	};
+ 
+	for (var i = 0; i < word.length; ++i ) {
+		if (converter[word[i]] == undefined){
+			answer += word[i];
+		} else {
+			answer += converter[word[i]];
+		}
+	}
+ 
+	return answer;
+}
+
+/* If only Cyrillic, then transliterate */
+function testWord(word) {
+  return (/^\p{sc=Cyrillic}*$/u.test(word)) ? translit(word) : word
+}
+
+const nameSpace = document.getElementById('firstName')
+
